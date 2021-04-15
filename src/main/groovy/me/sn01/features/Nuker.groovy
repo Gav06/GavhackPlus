669744da3aed4673de7d493f24ea3c9539f1e66b
sn0110 <69734357+sn0wy01@users.noreply.github.com>
2021-04-15 19:05:48 +1200

    start nuker and add groovy to buildscript

---
+++ src/main/groovy/me/sn01/features/Nuker.groovy
@@ -0,0 +1,11 @@
+package me.sn01.features
+
+import me.gavin.gavhackplus.feature.Category
+import me.gavin.gavhackplus.feature.Feature
+
+class Nuker extends Feature {
+    Nuker() {
+        super("Nuker", "Automatically mines blocks around you", Category.World)
+    }
+
+}
