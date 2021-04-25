8dc2291917188966cadfe91a1c5337a8549e5b09
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-04-25 00:34:26 -0700

    bruh

--- src/main/java/me/gavin/gavhackplus/feature/features/Freecam.java
+++ src/main/java/me/gavin/gavhackplus/feature/features/Freecam.java
@@ -5,7 +5,7 @@ import me.gavin.gavhackplus.feature.Feature;
 
 public class Freecam extends Feature {
     public Freecam() {
-        super("Freecam", "look around freely", Category.World);
+        super("Freecam", "Look around freely", Category.World);
     }
 
     
