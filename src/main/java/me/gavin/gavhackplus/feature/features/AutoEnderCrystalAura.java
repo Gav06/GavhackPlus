4419b47db748c48aef2f3355e5e34fdd15d11a1b
Gavin <72846934+gav06@users.noreply.github.com>
2021-04-27 11:42:50 -0400

    make autocrystal swag

--- src/main/java/me/gavin/gavhackplus/feature/features/AutoEnderCrystalAura.java
+++ src/main/java/me/gavin/gavhackplus/feature/features/AutoEnderCrystalAura.java
@@ -9,41 +9,54 @@ import me.gavin.gavhackplus.mixin.accessor.IMinecraft;
 import me.gavin.gavhackplus.setting.impl.BooleanSetting;
 import me.gavin.gavhackplus.setting.impl.NumberSetting;
 import me.gavin.gavhackplus.util.TickTimer;
+import net.minecraft.block.Block;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.item.EntityEnderCrystal;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
+import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
 import net.minecraft.network.play.client.CPacketUseEntity;
 import net.minecraft.network.play.server.SPacketSoundEffect;
 import net.minecraft.potion.Potion;
-import net.minecraft.util.CombatRules;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.EnumHand;
+import net.minecraft.util.*;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.Explosion;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AutoEnderCrystalAura extends Feature {
 
     private final NumberSetting attackDistance = new NumberSetting("AttackRange", this, 4.0f, 1.0f, 6.0f, 0.1f);
     private final NumberSetting placeDistance = new NumberSetting("PlaceRange", this, 4.0f, 1.0f, 6.0f, 0.1f);
+    private final NumberSetting minDmg = new NumberSetting("MinDmg", this, 4.0f, 0.1f, 10.0f, 0.1f);
+    private final NumberSetting maxSelfDmg = new NumberSetting("MaxSelfDmg", this, 15.0f, 1.0f, 30.0f, 0.1f);
     private final NumberSetting breakDelay = new NumberSetting("BreakDelay", this, 2.0f, 1.0f, 20.0f, 1.0f);
+    private final NumberSetting placeDelay = new NumberSetting("PlaceDelay", this, 2.0f, 1.0f, 20.0f, 1.0f);
     private final BooleanSetting setDead = new BooleanSetting("SetDead", this, true);
+    private final BooleanSetting fastPlace = new BooleanSetting("FastPlace", this, true);
 
-    private final TickTimer tickTimer;
+
+    private final TickTimer breakTickTimer;
+    private final TickTimer placeTickTimer;
 
     public AutoEnderCrystalAura() {
         super("AutoEnderCrystalAura", ":)", Category.Combat);
-        BooleanSetting fastPlace = new BooleanSetting("FastPlace", this, true);
-        addSettings(breakDelay, attackDistance, setDead, fastPlace);
-        tickTimer = new TickTimer();
+
+        addSettings(breakDelay, placeDelay, attackDistance, placeDistance, setDead, minDmg, maxSelfDmg, fastPlace);
+        breakTickTimer = new TickTimer();
+        placeTickTimer = new TickTimer();
     }
 
     private ArrayList<BlockPos> placedCrystals = new ArrayList<>();
@@ -52,8 +65,10 @@ public class AutoEnderCrystalAura extends Feature {
 
     @EventTarget
     public void onTick(TickEvent event) {
-        if (mc.player.getHeldItemMainhand().getItem() == Items.END_CRYSTAL) {
-            ((IMinecraft) mc).setDelayTimer(0);
+        if (fastPlace.getValue()) {
+            if (mc.player.getHeldItemMainhand().getItem() == Items.END_CRYSTAL) {
+                ((IMinecraft) mc).setDelayTimer(0);
+            }
         }
 
         doPlaceLogic();
@@ -83,24 +98,90 @@ public class AutoEnderCrystalAura extends Feature {
                 .filter(this::canAttackCrystal)
                 .findFirst().orElse(null);
 
-        if (canAttackCrystal(targetCrystal) && tickTimer.hasTicksPassed((int) breakDelay.getValue(), false)) {
+        if (canAttackCrystal(targetCrystal) && breakTickTimer.hasTicksPassed((int) breakDelay.getValue(), false)) {
             mc.player.swingArm(EnumHand.MAIN_HAND);
             mc.player.connection.sendPacket(new CPacketUseEntity(targetCrystal));
-            tickTimer.reset();
+            breakTickTimer.reset();
         }
     }
 
+    EntityPlayer targetPlayer;
+
     private void doPlaceLogic() {
         // finding target based on highest damage
-        float highestDamage = Float.MIN_VALUE;
+        List<BlockPos> crystalBlocks =
+                getBlocksAroundPlayer(placeDistance.getValue())
+                        .stream()
+                        .filter(this::canPlaceCrystal)
+                        .sorted(Comparator.comparing(blockPos -> mc.player.getDistanceSq(blockPos)))
+                        .collect(Collectors.toList());
+
+        BlockPos crystalPosition = null;
+
         for (Entity e : mc.world.loadedEntityList) {
-            if (e instanceof EntityPlayer) {
+            if (e instanceof EntityPlayer && e.getDistance(mc.player) <= placeDistance.getValue()) {
                 if (e.equals(mc.player))
                     continue;
 
-                //if (calculateDamage())
+                targetPlayer = (EntityPlayer) e;
+
+                double bestDamage = Double.MIN_VALUE;
+                BlockPos bestPos = null;
+
+                for (BlockPos pos : crystalBlocks) {
+                    // adding 0.5 to x for crystal width
+                    // adding 1.0 to y because crystal will be placed above
+                    double targetDamage = calculateDamage(pos.getX() + 0.5D, pos.getY() + 1, pos.getZ() + 0.5D, targetPlayer);
+                    double selfDmg = calculateDamage(pos.getX() + 0.5D, pos.getY() + 1, pos.getZ() + 0.5D, mc.player);
+
+                    if (targetDamage >= minDmg.getValue() && selfDmg <= maxSelfDmg.getValue()) {
+                        if (targetDamage > bestDamage) {
+                            bestDamage = targetDamage;
+                            bestPos = pos;
+                        }
+                    }
+                }
+
+                if (bestPos != null) {
+                    crystalPosition = bestPos;
+                }
             }
         }
+        if (crystalPosition != null) {
+            assert crystalPosition != null;
+            if (placeTickTimer.hasTicksPassed((int) placeDelay.getValue(), false)) {
+                mc.player.connection.sendPacket(new CPacketPlayerTryUseItemOnBlock(crystalPosition, EnumFacing.UP, EnumHand.MAIN_HAND, 0f, 0f, 0f));
+                mc.player.swingArm(EnumHand.MAIN_HAND);
+                placeTickTimer.reset();
+
+            }
+        }
+
+    }
+
+    private boolean canPlaceCrystal(BlockPos pos) {
+        // checking if blocks above are air
+        if (getBlock(pos.add(0, 1, 0)) == Blocks.AIR && getBlock(pos.add(0, 2, 0)) == Blocks.AIR) {
+
+            // seeing if it is obsidian or bedrock
+            if (getBlock(pos) == Blocks.OBSIDIAN || getBlock(pos) == Blocks.BEDROCK) {
+                // checking if nobody is standing directly above the block
+                BlockPos air1 = pos.add(0, 1, 0);
+                BlockPos air2 = pos.add(0, 2, 0);
+
+                return mc.world.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(air1)).isEmpty()
+                        && mc.world.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(air2)).isEmpty();
+
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private Block getBlock(BlockPos pos) {
+        return mc.world.getBlockState(pos).getBlock();
     }
 
     private boolean canAttackCrystal(EntityEnderCrystal targetCrystal) {
@@ -120,7 +201,8 @@ public class AutoEnderCrystalAura extends Feature {
             for (float y = range + 1; y > -range; y--) {
                 // loop for z axis
                 for (float z = -range; z < range; z++) {
-                    posList.add(new BlockPos(x, y, z));
+                    BlockPos pos = new BlockPos(x, y, z);
+                    posList.add(pos.add(mc.player.getPosition()));
                 }
             }
         }
@@ -167,5 +249,4 @@ public class AutoEnderCrystalAura extends Feature {
         }
         return (float) finald;
     }
-
 }
